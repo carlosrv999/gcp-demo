3a94c20c0228fc8d0eb85b8e0a9f7ff4beff8857
carlosrv999 <carlosrv125@icloud.com>
2020-05-07 03:35:27 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,19 +12,3 @@ ssh-keyscan -H $public_ip >> ~/.ssh/known_hosts
 ansible-playbook install-docker.yaml
 ansible-playbook install-java.yaml
 ansible-playbook install-plugins.yaml
-
-echo $(cat credentials.json) | sed -E 's/([^\]|^)"/\1\\"/g' > credentials.json.escaped
-
-jsonstring=$(cat credentials.json.escaped)
-
-curl -X POST 'http://'$public_ip':8080/credentials/store/system/domain/_/createCredentials' --data-urlencode 'json={
-  "": "0",
-  "credentials": {
-    "scope": "GLOBAL",
-    "id": "gcr-credentials",
-    "username": "_json_key",
-    "password": "'$jsonstring'"
-  }
-}'
-
-rm credentials.json.escaped
